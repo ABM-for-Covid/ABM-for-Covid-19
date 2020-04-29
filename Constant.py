c1c0ac19237b6616a3d8357a60bffdc9d2bbf6ff
codeashu <5406975+codeAshu@users.noreply.github.com>
2020-04-29 19:24:16 +0530

    changed conf

--- Constant.py
+++ Constant.py
@@ -1,8 +1,5 @@
 # variables ( Later put in a config file)
 from conf import env
-
-if env =="local":
-    home = "/Users/rusty/Desktop/covid-19/covid19_abm"
-else:
-    home = "/home/ubuntu/covid-19-Multiagent-Simulations"
+from conf import home
+home = conf.home
 
